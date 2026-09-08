b73670e2ed1408b59530f59da64b030a9d010107
Rebecca <rebeccacontact06@gmail.com>
2026-09-07 21:42:49 -0300

    Início dos testes de cálculo de pontuação

--- src/test/java/br/edu/unifacisa/impacta/model/CalculoPontuacaoTest.java
+++ src/test/java/br/edu/unifacisa/impacta/model/CalculoPontuacaoTest.java
@@ -2,9 +2,18 @@ package br.edu.unifacisa.impacta.model;
 
 import org.junit.jupiter.api.Test;
 
+import java.time.LocalDateTime;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
 class CalculoPontuacaoTest {
     @Test
     void deveCalcularPontuacaoDoPlantioPolimorficamente() {
+        AcaoSocioambiental acao = new PlantioMudas(1, "Plantio", "Plantio de girassol",LocalDateTime.of(2026,9,2,10,0
+        ), 10, 5 ); //criando um LocalDateTime com as informações q estou passando
+
+        assertEquals(15, acao.calcularPontuacao());
     }
 
     @Test
